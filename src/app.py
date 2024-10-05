3a064e2fdd2a199bffbf7f12bfae2afde2268706
Joshua Dirga <joshdirga@gmail.com>
2024-10-05 16:45:33 -0400

    Commit 2: db.py and app.py complete untested

---
+++ src/app.py
@@ -0,0 +1,343 @@
+import json
+from db import db
+from flask import Flask, request
+from db import User, Genre, Instrument, Mood, Query, Song
+from openai import OpenAI
+
+app = Flask(__name__)
+db_filename = "nocturne.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+
+
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code
+
+# ---------- User Routes ---------- #
+
+
+@app.route('/api/users/', methods=['GET'])
+def get_users():
+    """
+    Endpoint for getting all users
+    """
+    users = [user.serialize() for user in User.query.all()]
+    return success_response({"users": users})
+
+
+@app.route('/api/users/', methods=['POST'])
+def create_user():
+    """
+    Endpoint for creating a user
+    """
+    body = json.loads(request.data)
+    new_user = User(
+        username=body.get('username'),
+        email=body.get('email'),
+        hashed_password=body.get('hashed_password')
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+
+@app.route('/api/users/<int:user_id>/', methods=['GET'])
+def get_user(user_id):
+    """
+    Endpoint for getting a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    return success_response(user.serialize())
+
+
+@app.route('/api/users/<int:user_id>/', methods=['DELETE'])
+def delete_user(user_id):
+    """
+    Endpoint for deleting a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+# ---------- Song Routes ---------- #
+
+
+@app.route('/api/songs/', methods=['GET'])
+def get_songs():
+    """
+    Endpoint for getting all songs
+    """
+    songs = [song.serialize() for song in Song.query.all()]
+    return success_response({"songs": songs})
+
+
+@app.route('/api/songs/', methods=['POST'])
+def create_song():
+    """
+    Endpoint for creating a song
+    """
+    body = json.loads(request.data)
+    new_song = Song(
+        title=body.get('title'),
+        likes=body.get('likes', 0),
+        user_id=body.get('user_id'),
+        moods=[Mood.query.get(mood_id)
+               for mood_id in body.get('mood_ids', [])],
+        genres=[Genre.query.get(genre_id)
+                for genre_id in body.get('genre_ids', [])],
+        instruments=[Instrument.query.get(
+            instrument_id) for instrument_id in body.get('instrument_ids', [])]
+    )
+    db.session.add(new_song)
+    db.session.commit()
+    return success_response(new_song.serialize(), 201)
+
+
+@app.route('/api/songs/<int:song_id>/', methods=['GET'])
+def get_song(song_id):
+    """
+    Endpoint for getting a song
+    """
+    song = Song.query.filter_by(id=song_id).first()
+    if song is None:
+        return failure_response("Song not found")
+    return success_response(song.serialize())
+
+
+@app.route('/api/songs/<int:song_id>/', methods=['DELETE'])
+def delete_song(song_id):
+    """
+    Endpoint for deleting a song
+    """
+    song = Song.query.filter_by(id=song_id).first()
+    if song is None:
+        return failure_response("Song not found")
+    db.session.delete(song)
+    db.session.commit()
+    return success_response(song.serialize())
+
+# ---------- Genre Routes ---------- #
+
+
+@app.route('/api/genres/', methods=['GET'])
+def get_genres():
+    """
+    Endpoint for getting all genres
+    """
+    genres = [genre.serialize() for genre in Genre.query.all()]
+    return success_response({"genres": genres})
+
+
+@app.route('/api/genres/', methods=['POST'])
+def create_genre():
+    """
+    Endpoint for creating a genre
+    """
+    body = json.loads(request.data)
+    new_genre = Genre(
+        name=body.get('name')
+    )
+    db.session.add(new_genre)
+    db.session.commit()
+    return success_response(new_genre.serialize(), 201)
+
+
+@app.route('/api/genres/<int:genre_id>/', methods=['GET'])
+def get_genre(genre_id):
+    """
+    Endpoint for getting a genre
+    """
+    genre = Genre.query.filter_by(id=genre_id).first()
+    if genre is None:
+        return failure_response("Genre not found")
+    return success_response(genre.serialize())
+
+
+@app.route('/api/genres/<int:genre_id>/', methods=['DELETE'])
+def delete_genre(genre_id):
+    """
+    Endpoint for deleting a genre
+    """
+    genre = Genre.query.filter_by(id=genre_id).first()
+    if genre is None:
+        return failure_response("Genre not found")
+    db.session.delete(genre)
+    db.session.commit()
+    return success_response(genre.serialize())
+
+# ---------- Instrument Routes ---------- #
+
+
+@app.route('/api/instruments/', methods=['GET'])
+def get_instruments():
+    """
+    Endpoint for getting all instruments
+    """
+    instruments = [instrument.serialize()
+                   for instrument in Instrument.query.all()]
+    return success_response({"instruments": instruments})
+
+
+@app.route('/api/instruments/', methods=['POST'])
+def create_instrument():
+    """
+    Endpoint for creating an instrument
+    """
+    body = json.loads(request.data)
+    new_instrument = Instrument(
+        name=body.get('name')
+    )
+    db.session.add(new_instrument)
+    db.session.commit()
+    return success_response(new_instrument.serialize(), 201)
+
+
+@app.route('/api/instruments/<int:instrument_id>/', methods=['GET'])
+def get_instrument(instrument_id):
+    """
+    Endpoint for getting an instrument
+    """
+    instrument = Instrument.query.filter_by(id=instrument_id).first()
+    if instrument is None:
+        return failure_response("Instrument not found")
+    return success_response(instrument.serialize())
+
+
+@app.route('/api/instruments/<int:instrument_id>/', methods=['DELETE'])
+def delete_instrument(instrument_id):
+    """
+    Endpoint for deleting an instrument
+    """
+    instrument = Instrument.query.filter_by(id=instrument_id).first()
+    if instrument is None:
+        return failure_response("Instrument not found")
+    db.session.delete(instrument)
+    db.session.commit()
+    return success_response(instrument.serialize())
+
+# ---------- Mood Routes ---------- #
+
+
+@app.route('/api/moods/', methods=['GET'])
+def get_moods():
+    """
+    Endpoint for getting all moods
+    """
+    moods = [mood.serialize() for mood in Mood.query.all()]
+    return success_response({"moods": moods})
+
+
+@app.route('/api/moods/', methods=['POST'])
+def create_mood():
+    """
+    Endpoint for creating a mood
+    """
+    body = json.loads(request.data)
+    new_mood = Mood(
+        mood=body.get('mood')
+    )
+    db.session.add(new_mood)
+    db.session.commit()
+    return success_response(new_mood.serialize(), 201)
+
+
+@app.route('/api/moods/<int:mood_id>/', methods=['GET'])
+def get_mood(mood_id):
+    """
+    Endpoint for getting a mood
+    """
+    mood = Mood.query.filter_by(id=mood_id).first()
+    if mood is None:
+        return failure_response("Mood not found")
+    return success_response(mood.serialize())
+
+
+@app.route('/api/moods/<int:mood_id>/', methods=['DELETE'])
+def delete_mood(mood_id):
+    """
+    Endpoint for deleting a mood
+    """
+    mood = Mood.query.filter_by(id=mood_id).first()
+    if mood is None:
+        return failure_response("Mood not found")
+    db.session.delete(mood)
+    db.session.commit()
+    return success_response(mood.serialize())
+
+# ---------- Query Routes ---------- #
+
+
+@app.route('/api/queries/', methods=['GET'])
+def get_queries():
+    """
+    Endpoint for getting all queries
+    """
+    queries = [query.serialize() for query in Query.query.all()]
+    return success_response({"queries": queries})
+
+
+@app.route('/api/queries/', methods=['POST'])
+def create_query():
+    """
+    Endpoint for creating a query
+    """
+    body = json.loads(request.data)
+    new_query = Query(
+        query=body.get('query'),
+        user_id=body.get('user_id'),
+        moods=[Mood.query.get(mood_id)
+               for mood_id in body.get('mood_ids', [])],
+        genres=[Genre.query.get(genre_id)
+                for genre_id in body.get('genre_ids', [])],
+        instruments=[Instrument.query.get(
+            instrument_id) for instrument_id in body.get('instrument_ids', [])]
+    )
+    db.session.add(new_query)
+    db.session.commit()
+    return success_response(new_query.serialize(), 201)
+
+
+@app.route('/api/queries/<int:query_id>/', methods=['GET'])
+def get_query(query_id):
+    """
+    Endpoint for getting a query
+    """
+    query = Query.query.filter_by(id=query_id).first()
+    if query is None:
+        return failure_response("Query not found")
+    return success_response(query.serialize())
+
+
+@app.route('/api/queries/<int:query_id>/', methods=['DELETE'])
+def delete_query(query_id):
+    """
+    Endpoint for deleting a query
+    """
+    query = Query.query.filter_by(id=query_id).first()
+    if query is None:
+        return failure_response("Query not found")
+    db.session.delete(query)
+    db.session.commit()
+    return success_response(query.serialize())
+
+
+# to run flask app #
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- src/db.py
+++ src/db.py
@@ -2,6 +2,74 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+########## Association tables for many to many relationships ##########
+
+# Many to many relationship association table between User and Genre
+association_table2 = db.Table(
+    "user_genre_association",
+    db.Model.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("genre_id", db.Integer, db.ForeignKey("genres.id"))
+)
+
+# Many to many relationship association table between User and Instrument
+association_table3 = db.Table(
+    "user_instrument_association",
+    db.Model.metadata,
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id")),
+    db.Column("instrument_id", db.Integer, db.ForeignKey("instruments.id"))
+)
+
+# Many to many relationship association table between Query and Mood
+association_table4 = db.Table(
+    "query_mood_association",
+    db.Model.metadata,
+    db.Column("query_id", db.Integer, db.ForeignKey("queries.id")),
+    db.Column("mood_id", db.Integer, db.ForeignKey("moods.id"))
+)
+
+# Many to many relationship association table between Query and Genre
+association_table5 = db.Table(
+    "query_genre_association",
+    db.Model.metadata,
+    db.Column("query_id", db.Integer, db.ForeignKey("queries.id")),
+    db.Column("genre_id", db.Integer, db.ForeignKey("genres.id"))
+)
+
+# Many to many relationship association table between Query and Instrument
+association_table6 = db.Table(
+    "query_instrument_association",
+    db.Model.metadata,
+    db.Column("query_id", db.Integer, db.ForeignKey("queries.id")),
+    db.Column("instrument_id", db.Integer, db.ForeignKey("instruments.id"))
+)
+
+# Many to many relationship association table between Song and Mood
+association_table7 = db.Table(
+    "song_mood_association",
+    db.Model.metadata,
+    db.Column("song_id", db.Integer, db.ForeignKey("songs.id")),
+    db.Column("mood_id", db.Integer, db.ForeignKey("moods.id"))
+)
+
+# Many to many relationship association table between Song and Genre
+association_table8 = db.Table(
+    "song_genre_association",
+    db.Model.metadata,
+    db.Column("song_id", db.Integer, db.ForeignKey("songs.id")),
+    db.Column("genre_id", db.Integer, db.ForeignKey("genres.id"))
+)
+
+# Many to many relationship association table between Song and Instrument
+association_table9 = db.Table(
+    "song_instrument_association",
+    db.Model.metadata,
+    db.Column("song_id", db.Integer, db.ForeignKey("songs.id")),
+    db.Column("instrument_id", db.Integer, db.ForeignKey("instruments.id"))
+)
+
+########## Models ##########
+
 
 class User(db.Model):
     '''
@@ -12,6 +80,12 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     hashed_password = db.Column(db.String(120), nullable=False)
+    songs = db.relationship("Song", backref="user")
+    queries = db.relationship("Query", backref="user")
+    genres = db.relationship(
+        "Genre", secondary=association_table2, back_populates="users")
+    instruments = db.relationship(
+        "Instrument", secondary=association_table3, back_populates="users")
 
     def __init__(self, **kwargs):
         '''
@@ -28,7 +102,20 @@ class User(db.Model):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'songs': [song.serialize() for song in self.songs],
+            'genres': [genre.serialize() for genre in self.genres],
+            'instruments': [instrument.serialize() for instrument in self.instruments],
+        }
+
+    def simple_serialize(self):
+        '''
+        Serialize the User object without songs, genres, and instruments
+        '''
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
         }
 
 
@@ -41,6 +128,12 @@ class Song(db.Model):
     title = db.Column(db.String(80), nullable=False)
     likes = db.Column(db.Integer, nullable=False, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    moods = db.relationship(
+        "Mood", secondary=association_table7, back_populates="songs")
+    genres = db.relationship(
+        "Genre", secondary=association_table8, back_populates="songs")
+    instruments = db.relationship(
+        "Instrument", secondary=association_table9, back_populates="songs")
 
     def __init__(self, **kwargs):
         '''
@@ -58,5 +151,145 @@ class Song(db.Model):
             'id': self.id,
             'title': self.title,
             'likes': self.likes,
-            'user_id': self.user_id
+            'user_id': self.user_id,
+            'moods': [mood.serialize() for mood in self.moods],
+            'genres': [genre.serialize() for genre in self.genres],
+            'instruments': [instrument.serialize() for instrument in self.instruments],
+        }
+
+    def simple_serialize(self):
+        '''
+        Serialize the Song object without moods, genres, and instruments
+        '''
+        return {
+            'id': self.id,
+            'title': self.title,
+            'likes': self.likes,
+            'user_id': self.user_id,
+        }
+
+
+class Query(db.Model):
+    '''
+    Query model
+    '''
+    __tablename__ = 'queries'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    query = db.Column(db.String(80), default='')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    moods = db.relationship(
+        "Mood", secondary=association_table4, back_populates="queries")
+    genres = db.relationship(
+        "Genre", secondary=association_table5, back_populates="queries")
+    instruments = db.relationship(
+        "Instrument", secondary=association_table6, back_populates="queries")
+
+    def __init__(self, **kwargs):
+        '''
+        Initialize a Query object
+        '''
+        self.query = kwargs.get('query')
+        self.user_id = kwargs.get('user_id')
+
+    def serialize(self):
+        '''
+        Serialize the Query object
+        '''
+        return {
+            'id': self.id,
+            'query': self.query,
+            'user_id': self.user_id,
+            'moods': [mood.serialize() for mood in self.moods],
+            'genres': [genre.serialize() for genre in self.genres],
+            'instruments': [instrument.serialize() for instrument in self.instruments],
+        }
+
+########## Feature models ##########
+
+
+class Mood(db.Model):
+    '''
+    Mood model
+    '''
+    __tablename__ = 'moods'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    mood = db.Column(db.String(80), default='')
+    songs = db.relationship(
+        "Song", secondary=association_table7, back_populates="moods")
+    queries = db.relationship(
+        "Query", secondary=association_table4, back_populates="moods")
+
+    def __init__(self, **kwargs):
+        '''
+        Initialize a Mood object
+        '''
+        self.mood = kwargs.get('mood')
+
+    def serialize(self):
+        '''
+        Serialize the Mood object
+        '''
+        return {
+            'id': self.id,
+            'mood': self.mood,
+        }
+
+
+class Genre(db.Model):
+    '''
+    Genre model
+    '''
+    __tablename__ = 'genres'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    genre = db.Column(db.String(80), default='')
+    users = db.relationship(
+        "User", secondary=association_table2, back_populates="genres")
+    songs = db.relationship(
+        "Song", secondary=association_table8, back_populates="genres")
+    queries = db.relationship(
+        "Query", secondary=association_table5, back_populates="genres")
+
+    def __init__(self, **kwargs):
+        '''
+        Initialize a Genre object
+        '''
+        self.genre = kwargs.get('genre')
+
+    def serialize(self):
+        '''
+        Serialize the Genre object
+        '''
+        return {
+            'id': self.id,
+            'genre': self.genre,
+        }
+
+
+class Instrument(db.Model):
+    '''
+    Instrument model
+    '''
+    __tablename__ = 'instruments'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    instrument = db.Column(db.String(80), default='')
+    users = db.relationship(
+        "User", secondary=association_table3, back_populates="instruments")
+    songs = db.relationship(
+        "Song", secondary=association_table9, back_populates="instruments")
+    queries = db.relationship(
+        "Query", secondary=association_table6, back_populates="instruments")
+
+    def __init__(self, **kwargs):
+        '''
+        Initialize a Instrument object
+        '''
+        self.instrument = kwargs.get('instrument')
+
+    def serialize(self):
+        '''
+        Serialize the Instrument object
+        '''
+        return {
+            'id': self.id,
+            'instrument': self.instrument,
         }
